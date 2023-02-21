33c93bbd5966407e05da2c96082bc9b1df6c09b6
abhay02may <kumarabhay903@gmail.com>
2023-02-21 13:54:10 +0530

    Initial commit

---
+++ src/com/blockchain/crypto/Block.java
@@ -0,0 +1,4 @@
+package com.blockchain.crypto;
+
+public class Block {
+}
---
+++ src/com/blockchain/crypto/MainApp.java
@@ -0,0 +1,7 @@
+package com.blockchain.crypto;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
